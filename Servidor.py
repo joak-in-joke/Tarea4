ab26485acddda7defa6084a27e0f11c2d8dedcca
Joaquin Cancino <joaquin.canzalez@gmail.com>
2020-11-13 20:55:15 -0300

    Add files via upload

--- Servidor.py
+++ Servidor.py
@@ -1,4 +1,6 @@
+from phe import paillier
 import socket
+import pickle
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('localhost', 8089))
@@ -6,7 +8,19 @@ serversocket.listen(5) # become a server socket, maximum 5 connections
 
 while True:
     connection, address = serversocket.accept()
-    buf = connection.recv(64)
+    buf = connection.recv(4096)
+    data_variable = pickle.loads(buf)
     if len(buf) > 0:
-        print(buf)
+        print(data_variable)
+        public_key = data_variable
+        file1 = open('new_archivo_1', 'r') 
+        Lines = file1.readlines() 
+
+        file2 = open('cifrado_1', 'w') 
+
+        for line in Lines: 
+            file2.writelines(str(public_key.encrypt(line))+"\n")
+
+
+        file2.close() 
     break
